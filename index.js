8f193805c66c0a0857c7f87cd8ceed45cd165fb7
Zach <zawood11@gmail.com>
2021-10-06 19:05:19 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(arr, name){
+    return arr.filter(e => e.toUpperCase() === name.toUpperCase());
+}
+
+function fuzzyMatch(arr, search){
+    return arr.filter(e => e.charAt(0) === search.charAt(0));
+}
+
+function matchName(arr, search){
+    return arr.filter(e => e.name === search);
+}
